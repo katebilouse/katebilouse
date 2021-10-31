b21cc2d8179f3f6a396644aa4efee030666b9b52
Kate <katebilouse2gmail.com>
2021-10-31 10:43:57 +0200

    task 2 lesson3

---
+++ src/hillel/kate/Main2.java
@@ -0,0 +1,4 @@
+package hillel.kate;
+
+public class Main2 {
+}
---
+++ src/hillel/kate/Task2.java
@@ -0,0 +1,38 @@
+package hillel.kate;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        Scanner InputSideA = new Scanner(System.in);
+        System.out.println("Add a side:   ");
+        double firstside = InputSideA.nextInt();
+
+        Scanner InputSideB = new Scanner(System.in);
+        System.out.println("Add a side:   ");
+        double secondSide = InputSideA.nextInt();
+
+        Scanner InputSideC = new Scanner(System.in);
+        System.out.println("Add a side:   ");
+        double thirdSide = InputSideA.nextInt();
+
+        double Perimetr = getPerimetr(firstside,secondSide,thirdSide);
+        double SemiPerimetr = getSemiPerimetr(Perimetr);
+        double Square = getSquare(SemiPerimetr, firstside, secondSide, thirdSide);
+
+        System.out.println("Perimetr: " + Perimetr);
+        System.out.println("Half Perimetr:" + SemiPerimetr);
+        System.out.println("Square:"+ Square);
+    }
+
+    public static double getPerimetr(double firstSide,double secondSide,double thirdSide) {
+        return firstSide + secondSide + thirdSide;
+    }
+    public static double getSemiPerimetr(double getPerimetr){return (getPerimetr)/ 2;}
+
+    public static double getSquare(double SemiPerimetr, double firstSide,double secondside, double thirdSide){
+        return Math.sqrt(SemiPerimetr * (SemiPerimetr - firstSide) * (SemiPerimetr - secondside) * (SemiPerimetr - thirdSide));
+    }
+
+}
